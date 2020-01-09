bf4f5e1e7a1a106145875ed97a248589706075bb
Trunova Daria <trunova@gmail.com>
2020-01-09 17:07:26 +0200

    smth

---
+++ dfrProj/serializer/views.py
@@ -0,0 +1,35 @@
+from serializer.models import Snippet
+from serializer.serializers import SnippetSerializer, UserSerializer
+from rest_framework import generics
+from rest_framework import permissions
+
+from .permitions import IsOwnerOrReadOnly
+
+
+from django.contrib.auth.models import User
+
+
+class SnippetList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
